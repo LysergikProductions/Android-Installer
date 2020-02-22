fd6c9ed92b4d8db39fbf223d6832fa4a94442d3f
doge <doge@DESKTOP-982M1IB.localdomain>
2020-02-22 04:39:58 -0500

    Major and minor changes:
    Fixed critical bug of including -r in read functions corrupting file path variables
    
    Fixed; user can input OBB paths like "FOOcom." and APK paths like ".apkFOO"
    
    Fixed incorrectly calling MAIN instead of printHead when checking device connection for the first time fails
    
    Added checkVersion function that downloads the properties.txt and checks it against the current version
    
    Added waiting function that includes 'animated' strings
    
    Added INIT function for updating/downloading required packages
    
    Improved consistency throughout the UI experience
    
    Center-justified more UI stuff
    
    Replaced plain text title with an ASCII one via "toilet"
    
    Added automatic font size adjustment
    
    Changed RE1 (can't install) error into FE1 (made FE1 fatal)

--- AndroidInstall_1.1.3-release.sh
+++ AndroidInstall_1.1.3-release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#AndroidInstall_1.1.2-release.sh
+#AndroidInstall_1.1.3-release.sh
 #Nikolas A. Wagner © 2020
 #License: GNU GPLv3
 
@@ -7,30 +7,44 @@
 # Simplifies the process of installing builds on Android devices via Mac OSX using Android Debug Bridge
 
 #some global variables
-scriptName="AndroidInstall_1.1.2-release"; scriptTitle="*MONKEY INSTALLER*"; author="Nikolas A. Wagner"
-scriptVersion="1.1.2"; scriptVersionType="release"; license="GNU GPLv3"
+scriptName="AndroidInstall_1.1.3-release"; scriptTitle="*MONKEY INSTALLER*"; author="Nikolas A. Wagner"
+scriptVersion="1.1.3"; scriptVersionType="release"; license="GNU GPLv3"
 
-loopFromError="false"; errorMessage=" ..no error is saved here.. " deviceConnect="true"; adbVersion=$(adb version)
+loopFromError="false"; errorMessage=" ..no error is saved here.. " deviceConnect="false"; adbVersion=$(adb version)
 
-COLS=$(tput cols) #Text-UI elements and related variables
+COLS=$(tput cols) # Text-UI elements and related variables
 UIsep_title="------------------"; UIsep_head="-----------------------------------------"; UIsep_err0="--------------------------------"
 UItrouble="-- Troubleshooting --"
 
+# set font size on Mac OSX Terminal
+osascript -e "tell application \"Terminal\" to set the font size of window 1 to 15" >/dev/null 2>&1
+
+function INIT(){
+	clear; echo "Initializing.."; sleep 0.8
+	if toilet -h; then clear
+	else
+		printf "\n\nUpdating toilet:"
+		echo "cmd"
+		sleep 2
+		sudo apt install toilet
+	fi
+}
+
+INIT
+
 function printHead(){
 	if [ $loopFromError = "false" ]; then
-    		clear; printf "$scriptName\nby $author\n\n$adbVersion\nBash version ${BASH_VERSION}\n$UIsep_head\n\n"
+    	clear; printf "$scriptName\nby $author\n\n$adbVersion\nBash version ${BASH_VERSION}\n$UIsep_head\n\n"
 	elif [ $loopFromError = "true" ]; then
-    		clear; printf "$scriptName\nby $author\n\n$adbVersion\nBash version ${BASH_VERSION}\n$UIsep_head\n\n"
-    		printf "$errorMessage\n\n"
+		clear; printf "$scriptName\nby $author\n\n$adbVersion\nBash version ${BASH_VERSION}\n$UIsep_head\n\n"
+		printf "$errorMessage\n\n"
 
-    		if [ $deviceConnect = "false" ]; then
-			until adb shell exit; do
-				clear; printf "$scriptName\nby $author\n\n$adbVersion\nBash version ${BASH_VERSION}\n$UIsep_head\n\n"
-    				printf "$errorMessage\n\n\n -- waiting for device --\n"
-				printf " ."; sleep 1; printf " ."; sleep 1; printf " ."; sleep 1
+		if [ $deviceConnect = "false" ]; then
+			until adb shell exit >/dev/null 2>&1; do
 				export deviceConnect="false"
+				waiting
 			done
-			export deviceConnect="true"
+			printf "\r%*s\n\n" $[$COLS/2] "Device Connected!   "
 		elif [ $deviceConnect = "true" ]; then
 			echo
 		else
@@ -38,7 +52,7 @@ function printHead(){
 			export deviceConnect="true"
 			exit status 1
 		fi
-		echo
+		export deviceConnect="true"
   	else
     		export errorMessage="$errorMessage\n\n$UIsep_err0\n\n"
     		export errorMessage+="ER1 - Script restarted; 'loopFromError' had an unexpected value."
@@ -48,22 +62,21 @@ function printHead(){
 		printf "5.. "; sleep 1; printf "4.. "; sleep 1; printf "3.. "; sleep 1; printf "2.. "; sleep 1; printf "1.. "; sleep 1
 		MAIN
   	fi
+  	printf "\nMounting device...\n\n"; adb devices
 }
 
 function printTitle(){
-	printf "\n%*s\n" $[$COLS/2] "$scriptTitle"
-	printf "%*s\n\n" $[$COLS/2] "$UIsep_title"
+	toilet -t -F gay "Monkey Installer"
+	#printf "\n%*s\n" $[$COLS/2] "$title"
+	#printf "%*s\n\n\n" $[$COLS/2] "$UIsep_title"
 }
 
 function MAIN(){
+	printHead; checkConnect; printTitle
+
 	#checking for fatal error while calling the main functions of the script
 	if {
-		printHead; printf "\nMounting device...\n\n"
-		adbWAIT; adb devices
-		printTitle
-		getOBB; adbWAIT
-		getAPK; adbWAIT
-		INSTALL
+		getOBB; adbWAIT; getAPK; adbWAIT; INSTALL
 	}; then printf "\nGoodbye!\n"; echo; exit
 	else
 		printf "\nFE0 - Fatal Error; problem calling main functions.\nPlease report this error code to Nick.\n"; sleep 1
@@ -73,30 +86,44 @@ function MAIN(){
 }
 
 #Check for device connection; reset script in case of error
-function adbWAIT(){ #update the script on status of adb connection
+function checkConnect(){	
 	if (adb shell settings put global development_settings_enabled 1); then
 		export deviceConnect="true"
 	else
 		loopFromError="true"; deviceConnect="false"
 		export errorMessage="RE0 - No devices found, or found more than one connected.\n\n"
-    		export errorMessage+="             $UItrouble\n"
-    		export errorMessage+="Ensure only one device is connected and that is has USB Debugging permissions..\n"
-    		export errorMessage+="For more help on this, search 'ADB fixAll' in google drive."
+    	export errorMessage+="				$UItrouble\n"
+    	export errorMessage+="Ensure only one device is connected and that is has USB Debugging permissions..\n"
+    	export errorMessage+="For more help on this, search 'ADB fixAll' in google drive."
 
-		sleep 1; printf "\nRE0 - Could not connect to just one device; resetting script in..\n"; sleep 1
-		printf "5.. "; sleep 1; printf "4.. "; sleep 1; printf "3.. "; sleep 1; printf "2.. "; sleep 1; printf "1.. "; sleep 1
-		MAIN
+		echo; printf "\nRE0 - Could not connect to just one device; resetting script in..\n"; sleep 0.5
+		printf "5.. "; sleep 1; printf "4.. "; sleep 1; printf "3.. "; sleep 1; printf "2.. "; sleep 1; printf "1.. "; sleep 0.5
+		echo; echo "debug back to printHead"; sleep 1
+		printHead
+	fi
+}
+
+#update the script on status of adb connection
+function adbWAIT(){
+	if (adb shell exit >/dev/null 2>&1); then
+		export deviceConnect="true"
+	else
+		until (adb shell exit >/dev/null 2>&1); do waiting; done
+		export deviceConnect="true"
+		printf "\r%*s\n\n" $[$COLS/2] "Device Connected!   "
+		printTitle
 	fi
 }
 
 function getOBB(){ #this function gets the OBB name needed to isolate the monkey events to the app being tested
-	read -r -p 'Drag OBB anywhere in here: ' OBBfilePath #i.e. Server:\folder\ folder/folder/com.studio.platform.appName
+	printf "\n%*s\n" $[$COLS/2] "Drag OBB anywhere here:"
+	read -p '' OBBfilePath #i.e. Server:\folder\ folder/folder/com.studio.platform.appName
 	if [ "$OBBfilePath" == "" ]; then
 		export OBBvalid="false"
 		printHead; printTitle
-		printf "\n%*s\n" $[$COLS/2] "You forgot to drag the OBB!"
+		printf "%*s\n" $[$COLS/2] "You forgot to drag the OBB!"
 		getOBB
-	elif [[ ! "$OBBfilePath" == *"com."* ]]; then
+	elif [[ ! "$OBBfilePath" == "com."* ]]; then
 		export OBBvalid="false"
 	elif [ "$OBBfilePath" == "fire" ]; then
 		export OBBvalid="true"
@@ -122,15 +149,16 @@ function getOBB(){ #this function gets the OBB name needed to isolate the monkey
 }
 
 function getAPK(){
-	APKvalid="true"; echo
-	read -r -p 'Drag APK anywhere in here: ' APKfilePath
+	APKvalid="true"
+	printf "%*s\n" $[$COLS/2] "Drag APK anywhere here:"
+	read -p '' APKfilePath
 
 	if [ "$APKfilePath" == "" ]; then
 		printHead; printTitle
 		export APKvalid="false"
 		printf "%*s\n\n" $[$COLS/2] "You forgot to drag the APK!"
 		getAPK
-	elif [[ "$APKfilePath" == *".apk"* ]]; then
+	elif [[ "$APKfilePath" == *".apk" ]]; then
 		export APKvalid="true"
 		local cleanPath="${APKfilePath#*:*}"
 		export APKname=$(basename "$cleanPath")
@@ -151,10 +179,8 @@ function getAPK(){
 
 function INSTALL(){
 	if {
-		printf "\nUploading OBB..\n"
-		adb push "$OBBfilePath" /sdcard/Android/OBB
-		printf "\nInstalling APK..\n"
-		adb install --no-streaming "$APKfilePath"
+		printf "\nUploading OBB..\n"; adb push "$OBBfilePath" /sdcard/Android/OBB
+		printf "\nInstalling APK..\n"; adb install --no-streaming "$APKfilePath"
 	}; then
 		printf "\n\nLaunching app."
 		adb shell "$launchCMD" > /dev/null 2>&1; sleep 1; printf " ."; sleep 1; printf " .\n"
@@ -162,24 +188,57 @@ function INSTALL(){
 		errorMessage="Any previous error messages will be printed to a log at this time in the next release!"
 		printf "\nGoodbye!\n\n"; exit
 	else
-		export loopFromError="true"
-    		export errorMessage="RE1 - Fatal error while executing INSTALL function.\n\n"
-    		export errorMessage+="             $UItrouble\n"
-    		export errorMessage+="Ensure only one device is connected and that is has USB Debugging permissions..\n"
-    		export errorMessage+="For more help on this, search for ADB fixAll in google drive."
-
-		sleep 1; printf "\nRE1 - The install process could not be executed; resetting script in..\n"; sleep 1
+		printf "\nFE1 - Fatal Error; install was unsuccesful for unknown reasons.\nPlease report this error code to Nick.\n"; sleep 1
 		printf "5.. "; sleep 1; printf "4.. "; sleep 1; printf "3.. "; sleep 1; printf "2.. "; sleep 1; printf "1.. "; sleep 1
-
-		MAIN
+		echo; exit 1
 	fi
 }
 
 function checkVersion(){
-	#clone Android-Install repo and mv properties.txt into working directory of this script
+	clear
+	{ #clone repo; if error then update with git pull
+		pushd ~ > /dev/null 2>&1; git clone https://github.com/LysergikProductions/Android-Installer.git > /dev/null 2>&1; popd > /dev/null 2>&1 ||
+		pushd ~/Android-Installer > /dev/null 2>&1; git pull > /dev/null 2>&1; popd > /dev/null 2>&1
+	}
 	currentVersion=$(grep -n "_version " properties.txt); export currentVersion="${currentVersion##* }"
-	if [ "$scriptVersion" == "$currentVersion" ]; then echo "This script is up-to-date!"; else echo "Update required.."; fi
+	
+	printf "\n\n\n\n\n%*s\n" $[$COLS/2] "This script: v$scriptVersion"
+	printf "%*s\n" $[$COLS/2] "Latest version: v$currentVersion"
+
+	if [ "$scriptVersion" == "$currentVersion" ]; then
+		printf "\n%*s" $[$COLS/2] "This script is up-to-date!"
+	else printf "\n%*s" $[$COLS/2] "Update required..."; fi
+}
+checkVersion; sleep 2
+
+function waiting(){
+	waitMessage="-- waiting for device --"
+	local anim1=( "" " ." " . ." " . . ." " . . . ." " . . . . ." )
+	local anim2=(
+	"oooooooooooooooooooooooo"
+	"Oooooooooooooooooooooooo" "oOoooooooooooooooooooooo" "ooOooooooooooooooooooooo" "oooOoooooooooooooooooooo" "ooooOooooooooooooooooooo" "oooooOoooooooooooooooooo"
+	"ooooooOooooooooooooooooo" "oooooooOoooooooooooooooo" "ooooooooOooooooooooooooo" "oooooooooOoooooooooooooo" "ooooooooooOooooooooooooo" "oooooooooooOoooooooooooo"
+	"ooooooooooooOooooooooooo" "oooooooooooooOoooooooooo" "ooooooooooooooOooooooooo" "oooooooooooooooOoooooooo" "ooooooooooooooooOooooooo" "oooooooooooooooooOoooooo"
+	"ooooooooooooooooooOooooo" "oooooooooooooooooooOoooo" "ooooooooooooooooooooOooo" "oooooooooooooooooooooOoo" "ooooooooooooooooooooooOo" "oooooooooooooooooooooooO"
+	)
+
+	clear; printf "$scriptName\nby $author\n\n$adbVersion\nBash version ${BASH_VERSION}\n$UIsep_head\n\n"
+    printf "$errorMessage\n\n\n"
+    printf "\n%*s\n" $[$COLS/2] "$waitMessage"
+
+	for i in "${anim2[@]}"
+	do
+		printf "\r%*s" $[$COLS/2] "$i"
+		sleep 0.1
+	done
 }
 
-#sleep 2 | checkVersion
-MAIN
+{ # try to run the script
+    MAIN &&
+    printf "\nDebug: MAIN completed without errors!\n"
+} || {
+    printf "\nDebug: MAIN is caught having an error!\n"
+}
+
+#Say goodbye when done everything, regardless of an exit
+printf "\nGoodbye!\n"; exit
