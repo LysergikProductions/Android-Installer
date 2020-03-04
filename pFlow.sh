c3d2ec99b536b7b4393ae7b337ea106ec5fd4699
Nikolas A Wagner <nwagner@M024-nwagner.local>
2020-03-04 10:15:39 -0500

    Fixed encoding error in new sh file containing program flow

---
+++ pFlow.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+getOBB(){
+  echo "getOBB completes successfully"; echo
+  sleep 1
+}
+
+getAPK(){
+  echo "getAPK completes successfully"; echo
+  sleep 1; INSTALL
+}
+
+INSTALL(){
+  (echo "adb uninstall"); sleep 1
+  
+  (
+    printf "\nUploading OBB..\n"
+    (echo "pushOBB succeeds") && exit || echo "catch error in INSTALL"; echo; exit 1
+  )
+
+  sleep 1
+
+  (
+    printf "\nInstalling APK..\n"
+    (echo "install APK fails"; exit) && exit 1 || echo "catch error in INSTALL"; echo; exit 1
+  )
+}
+
+MAIN(){
+  getOBB
+  (getAPK) && exit || sleep 1; printf "\nMAIN: catch error in MAIN\n"; exit 1
+}
+
+(MAIN) || printf "FINAL: catch error in error handling\n"
+printf "\nGoodbye!\n"; exit
