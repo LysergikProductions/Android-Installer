67bdcc175d2d1c25f3616d99a18ad2d0ac5807c7
bDoge <lysergik.productions@gmail.com>
2020-03-25 01:09:04 -0400

    minor changes

--- AndroidInstall_1.2.2-beta.sh
+++ AndroidInstall_1.2.2-beta.sh
@@ -3,7 +3,7 @@
 # 2020 (C) Nikolas A. Wagner
 # License: GNU GPLv3
 
-# Build_0316
+# Build_0317
 
 	#This program is free software: you can redistribute it and/or modify
 	#it under the terms of the GNU General Public License as published by
@@ -34,7 +34,7 @@ if ! file /tmp/variables.before 1>/dev/null; then kill $( jobs -p ) 2>/dev/null 
 # some global variables
 scriptStartDate=""; scriptStartDate=$(date)
 
-build="0316"; scriptVersion=1.2.0-release; author="Nikolas A. Wagner"; license="GNU GPLv3"
+build="0317"; scriptVersion=1.2.0-release; author="Nikolas A. Wagner"; license="GNU GPLv3"
 scriptTitleDEF="StoicDroid"; scriptPrefix="AndroidInstall_"; scriptFileName=$(basename "$0")
 adbVersion=$(adb version); bashVersion=${BASH_VERSION}; currentVersion="_version errorGettingProperties.txt"
 
@@ -647,8 +647,8 @@ getOBB(){
 	COLS=$(tput cols)
 
 	if [ "$qMode" = "false" ]; then
-		printf "\n%*s\n" $((COLS)) "Enter 'tool' to access the toolkit  "
-		printf "%*s\n" $((0)) "$OBBquest"
+		printf "\n\t\t%*s\n" $((COLS/2)) "Enter 'tool' to access the toolkit"
+		printf "\n%*s\n" $((0)) "$OBBquest"
 		printf "$OBBinfo"
 	else
 		printf "%*s\n" $((COLS/2)) "$OBBquest"; printf "$OBBinfo"
